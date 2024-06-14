9cb9a85d83acaad6c1b124fbb2b55ef8c90d8a13
kaskad-saim <saim.asutp@gmail.com>
2024-06-14 10:01:35 +0500

    rework kran

--- js/pechVr-mnemo.js
+++ js/pechVr-mnemo.js
@@ -217,3 +217,74 @@ if (temper1Skolz.innerHTML < 550 && temper1Skolz.innerHTML > 50) {
   modeTitle.innerHTML = 'Печь не работает';
 }
 //------------------------------------------------------------------
+//Краны
+const kran = document.querySelector('.mnemo__kran-img img');
+const kranLeft1 = document.querySelector('.mnemo__kran1-left');
+const kranRight1 = document.querySelector('.mnemo__kran1-right');
+
+const kranBorderLeft = (param, color) => {
+  param.style = `border-left: 13px solid ${color}`;
+};
+
+const kranBorderRight = (param, color) => {
+  param.style = `border-right: 13px solid ${color}`;
+};
+
+const green = 'green';
+const red = 'red';
+
+if (kran.src == 'http://techsite4/KASKAD/pic/images/true.gif') {
+  kranBorderLeft(kranLeft1, green);
+  kranBorderRight(kranRight1, green);
+} else {
+  kranBorderLeft(kranLeft1, red);
+  kranBorderRight(kranRight1, red);
+}
+
+const kran2 = document.querySelector('.mnemo__kran2-img img');
+const kranLeft2 = document.querySelector('.mnemo__kran2-left');
+const kranRight2 = document.querySelector('.mnemo__kran2-right');
+
+if (kran2.src == 'http://techsite4/KASKAD/pic/images/true.gif') {
+  kranLeft2.style = `border-left: 12px solid green`;
+  kranRight2.style = `border-right: 12px solid green`;
+} else {
+  kranLeft2.style = `border-left: 12px solid red`;
+  kranRight2.style = `border-right: 12px solid red`;
+}
+
+const kran3 = document.querySelector('.mnemo__kran3-img img');
+const kranLeft3 = document.querySelector('.mnemo__kran3-left');
+const kranRight3 = document.querySelector('.mnemo__kran3-right');
+
+if (kran3.src == 'http://techsite4/KASKAD/pic/images/true.gif') {
+  kranBorderLeft(kranLeft3, green);
+  kranBorderRight(kranRight3, green);
+} else {
+  kranBorderLeft(kranLeft3, red);
+  kranBorderRight(kranRight3, red);
+}
+
+const kran4 = document.querySelector('.mnemo__kran4-img img');
+const kranLeft4 = document.querySelector('.mnemo__kran4-left');
+const kranRight4 = document.querySelector('.mnemo__kran4-right');
+
+if (kran4.src == 'http://techsite4/KASKAD/pic/images/true.gif') {
+  kranBorderLeft(kranLeft4, green);
+  kranBorderRight(kranRight4, green);
+} else {
+  kranBorderLeft(kranLeft4, red);
+  kranBorderRight(kranRight4, red);
+}
+
+const kran5 = document.querySelector('.mnemo__kran5-img img');
+const kranLeft5 = document.querySelector('.mnemo__kran5-left');
+const kranRight5 = document.querySelector('.mnemo__kran5-right');
+
+if (kran5.src == 'http://techsite4/KASKAD/pic/images/true.gif') {
+  kranBorderLeft(kranLeft5, green);
+  kranBorderRight(kranRight5, green);
+} else {
+  kranBorderLeft(kranLeft5, red);
+  kranBorderRight(kranRight5, red);
+}

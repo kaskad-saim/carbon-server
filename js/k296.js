e3262a46bf7cfc1056f294aa9e18a0c10cc2fb1d
kaskad-saim <saim.asutp@gmail.com>
2024-07-09 09:16:18 +0500

    add k296 modal

--- js/k296.js
+++ js/k296.js
@@ -15,3 +15,66 @@ hoverNoneBtn.addEventListener('click', () => {
   }
   toggleBtnText();
 });
+
+
+
+// --------------------Модальное окно--------------------
+
+const btnModal = document.querySelector('.btn-modal');
+const modalBackground = document.querySelector('.modal-js');
+const modalClose = document.querySelector('.mnemo__modal-close');
+const modalActive = document.querySelector('.mnemo__modal-active');
+
+const accordionBtn = document.querySelectorAll('.modal__accordion');
+const accordionTitle = document.querySelectorAll('.modal__accordion-title');
+const accordionContent = document.querySelectorAll('.modal__accordion-content');
+
+btnModal.addEventListener('click', () => {
+  modalBackground.classList.add('enabled');
+  modalActive.classList.add('enabled');
+});
+
+modalClose.addEventListener('click', () => {
+  modalBackground.classList.remove('enabled');
+  modalActive.classList.remove('enabled');
+});
+
+modalBackground.addEventListener('click', (event) => {
+  if (event.target === modalBackground) {
+    modalBackground.classList.remove('enabled');
+    modalActive.classList.remove('enabled');
+  }
+});
+
+const dropDownDescrNull = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    const el = array[i];
+    el.style.maxHeight = null;
+  }
+};
+
+for (let i = 0; i < accordionTitle.length; i++) {
+  const el = accordionTitle[i];
+
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    let contentNext = el.nextElementSibling;
+
+    if (contentNext.style.maxHeight) {
+      dropDownDescrNull(accordionContent);
+    } else {
+      dropDownDescrNull(accordionContent);
+      contentNext.style.maxHeight = contentNext.scrollHeight + 'px';
+    }
+
+    if (!el.classList.contains('enabled')) {
+      for (let i = 0; i < accordionTitle.length; i++) {
+        let item = accordionTitle[i];
+        item.classList.remove('enabled');
+      }
+      el.classList.add('enabled');
+    } else {
+      el.classList.remove('enabled');
+    }
+  });
+}
